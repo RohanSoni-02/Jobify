f88be2fd3ac914288e8ee863662ab5f99077651e
RohanSoni-02 <soni.rohanchd@gmail.com>
2025-04-17 22:14:14 +1000

    added links

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div>Register</div>
+    <Link to="/register">Register page </Link>
+    </>
   )
 }
 
